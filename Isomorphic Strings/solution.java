5e9f2d2a7830a2a770eded7bc71bc20711cdf37e
yuvisu <huanghuoyu@gmail.com>
2017-04-12 06:21:31 +0800

    35%

---
+++ Isomorphic Strings/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<Character,Character>();
+        for(int i =0;i<s.length();i++){
+            
+            if(map.get(s.charAt(i))==null){
+                if(map.containsValue(t.charAt(i))) return false;
+                map.put(s.charAt(i),t.charAt(i));
+            }
+            
+            if(map.get(s.charAt(i))!=t.charAt(i)) return false;
+            
+        }
+        return true;
+    }
+}
